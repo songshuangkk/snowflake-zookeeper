f683b9524fef98c6fccd55db7e1cd2c6ffb6a172
songshuang <songshuang@babytree-inc.com>
2019-02-19 21:45:17 +0800

    first commit

---
+++ src/main/java/com/songshuang/snowflake/SnowflakeIdApplication.java
@@ -0,0 +1,13 @@
+package com.songshuang.snowflake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowflakeIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowflakeIdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/songshuang/snowflake/SnowflakeIdApplicationTests.java
@@ -0,0 +1,16 @@
+package com.songshuang.snowflake;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SnowflakeIdApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
