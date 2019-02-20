caf045ede4d6539818d1b54b30cc610190ba0b3b
songshuangkk <songshuangkk@gmail.com>
2019-02-20 22:49:17 +0800

    代码提交

--- src/main/java/com/songshuang/snowflake/init/ZkInit.java
+++ src/main/java/com/songshuang/snowflake/init/ZkInit.java
@@ -1,8 +1,11 @@
 package com.songshuang.snowflake.init;
 
+import com.google.common.base.Joiner;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.NodeCache;
+import org.apache.curator.framework.recipes.cache.NodeCacheListener;
 import org.apache.curator.retry.RetryForever;
 import org.apache.zookeeper.CreateMode;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +14,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ZkInit implements CommandLineRunner {
+
+  private static final String PARENT_PATH = "/snowFlake";
 
   @Value("${zookeeper.address}")
   private String zookeeperAddress;
@@ -24,20 +29,22 @@ public class ZkInit implements CommandLineRunner {
 
     curatorClint.start();
 
-//    if (curatorClint.checkExists().forPath("/snowFlake") != null) {
-//      System.out.println("Path SnowFlake exists.");
-//      curatorClint.delete().forPath("/snowFlake");
-//    } else {
-//      System.out.println("Create Path SnowFlake.");
-//      curatorClint.create().forPath("/snowFlake");
-//    }
+    if (curatorClint.checkExists().forPath(PARENT_PATH) != null) {
+      System.out.println("Path SnowFlake exists.");
+      curatorClint.delete().forPath(PARENT_PATH);
+    } else {
+      System.out.println("Create Path SnowFlake.");
+      curatorClint.create().forPath("/snowFlake");
+    }
 
+    int size = curatorClint.getChildren().forPath(PARENT_PATH).size();
+    // 遍历所有的子节点
+    curatorClint.getChildren().forPath(PARENT_PATH).forEach(item -> {
+
+    });
     // 通过在父节点上创界临时节点来变更
+    curatorClint.create().withMode(CreateMode.EPHEMERAL).forPath(Joiner.on("/").join(PARENT_PATH, size));
 
-    curatorClint.getChildren().forPath("/snowFlake");
-
-    curatorClint.create().withMode(CreateMode.EPHEMERAL).forPath("/snowFlake/child");
-
-    curatorClint.getChildren().forPath("/snowFlake");
+    // watch child change
   }
 }
